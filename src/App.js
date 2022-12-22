d8b5ec7fcae2d86e77ddc62d4a863360c4599fed
MichaelOb89 <michaelobrien.t@hotmail.com>
2022-12-21 19:26:11 -0500

    SEI Cafe start

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import './App.css';
-import { useState, useEffect } from 'react';
+// import { useState, useEffect } from 'react';
+// import AuthPage from './pages/AuthPage/AuthPage';
+// import NewOrderPage from './pages/NewOrderPage/NewOrderPage';
+// import OrderHistoryPage from './pages/OrderHistoryPage/OrderHistoryPage';
+// import Nav from './components/Nav';
+// import {Routes, Route} from 'react-router-dom'
+// import FruitsPage from './pages/FruitsPage/FruitsPage';
 
-function App() {
-  const[state, setState] = useState(null)
-  const fetchState = async () => {
-    try{
-      const response = await fetch('/api/test')
-      const data = await response.json()
-      setState(data)
-    } catch(err){
-      console.error(err)
-    }
-  }
+// function App() {
+//   const[state, setState] = useState(null)
+//   const [user, setUser] = useState(null)
+//   const fetchState = async () => {
+//     try{
+//       const response = await fetch('/api/test')
+//       const data = await response.json()
+//       setState(data)
+//     } catch(err){
+//       console.error(err)
+//     }
+//   }
 
-  useEffect(() => {
-    fetchState()
-  }, [])
-  return (
-    <div className="App">
-      {state && state.eureka ? <>{state.eureka}</> : <>You are still Looking. Don't give up</>}
-    </div>
-  );
-}
+//   useEffect(() => {
+//     fetchState()
+//   }, [])
+//   return (
+//     <main className='App'>
+//       { user ?
+//         <>
+//           <Nav />
+//           <Routes>
+//             <Route path='/fruits' element={<FruitsPage />}/>
+//             <Route path='/orders/new' element={<NewOrderPage />}/>
+//             <Route path='/orders' element={<OrderHistoryPage />}/>
+//           </Routes>
+//         </> :
+//           <AuthPage />
+        
+//       }
+//     </main>
+//   );
+// }
 
-export default App;
+// export default App;
---
+++ src/pages/App/App.js
@@ -0,0 +1,45 @@
+import { useState, useEffect } from 'react';
+import AuthPage from '../AuthPage/AuthPage';
+import NewOrderPage from '../NewOrderPage/NewOrderPage';
+import OrderHistoryPage from '../OrderHistoryPage/OrderHistoryPage';
+import Nav from '../../components/NavBar/NavBar';
+import {Routes, Route} from 'react-router-dom'
+import FruitsPage from '../FruitsPage/FruitsPage';
+
+function App() {
+  const[state, setState] = useState(null)
+  const [user, setUser] = useState(null)
+  const fetchState = async () => {
+    try{
+      const response = await fetch('/api/test')
+      const data = await response.json()
+      setState(data)
+    } catch(err){
+      console.error(err)
+    }
+  }
+
+  useEffect(() => {
+    fetchState()
+  }, [])
+  
+  return (
+    <main className='App'>
+      { user ?
+        <>
+          <Nav />
+          <Routes>
+            <Route path='/fruits' element={<FruitsPage />}/>
+            <Route path='/orders/new' element={<NewOrderPage />}/>
+            <Route path='/orders' element={<OrderHistoryPage />}/>
+            <Route path='/' element={<NewOrderPage />}/>
+          </Routes>
+        </> :
+          <AuthPage setUser={setUser}/>
+        
+      }
+    </main>
+  );
+}
+
+export default App;
